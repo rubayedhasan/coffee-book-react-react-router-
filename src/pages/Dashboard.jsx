2126c80d4ec70eb642bb054b9a3f619bb73bdd80
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-12-18 00:31:12 +0600

    render heading component & Coffee component to the dashboard component base on save data on local storage

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import Coffee from "../components/Coffee";
+import Heading from "../components/Heading";
+import { getTheFavorites } from "../utils/coffeeStorage";
+
 const Dashboard = () => {
+  const [coffees, setCoffees] = useState([]);
+
+  // Effect:: for load the save favorites data from local storage
+  useEffect(() => {
+    const favorites = getTheFavorites();
+    setCoffees(favorites);
+  }, []);
+
   return (
-    <div>
-      <p>Dashboard</p>
-    </div>
+    <>
+      {/* heading  */}
+      <Heading
+        title="Welcome to Dashboard"
+        subTitle="Manage coffees that you have previously added as favorite. You can view or remove them from here."
+      ></Heading>
+
+      {/* selected coffee card  */}
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 place-items-center gap-8 mt-8 md:mt-10 lg:mt-12">
+        {coffees.map((coffee) => (
+          <Coffee key={coffee.id} coffee={coffee}></Coffee>
+        ))}
+      </div>
+    </>
   );
 };
 
